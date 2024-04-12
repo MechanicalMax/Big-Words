57a51882c3061c92c6a471e48750c63183442928
Maximus Shurr <maxshurr@gmail.com>
2024-04-12 19:10:47 -0400

    Dispose on close and hide cursor

--- BigWords.java
+++ BigWords.java
@@ -1,13 +1,19 @@
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.Point;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.WindowConstants;
 
 public class BigWords {
     public static void main(String[] args) {
+        // Create JFrame
         JFrame frame = new JFrame("Big Words");
 
+        // Create Label
         JLabel textLabel = new JLabel("Hello");
         textLabel.setHorizontalAlignment(JLabel.CENTER);
         textLabel.setFont(new Font("SansSerif", Font.BOLD, 90));
@@ -15,10 +21,17 @@ public class BigWords {
 
         frame.add(textLabel);
 
+        // Maximize Window and Remove Cursor
         frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
         frame.setUndecorated(true);
         frame.getContentPane().setBackground(Color.BLACK);
+        frame.getContentPane().setCursor(Toolkit.getDefaultToolkit().createCustomCursor(
+            new BufferedImage(16, 16, BufferedImage.TYPE_INT_ARGB),
+            new Point(0, 0), "blank cursor"));
 
+        // Display Frame to User
+        frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        
         frame.setVisible(true);
     }
 }
