d798de402b7ba62daa9c0bdf1a126d670a9f468e
Maximus Shurr <maxshurr@gmail.com>
2024-04-12 17:10:48 -0400

    Add textLabel for words

--- BigWords.java
+++ BigWords.java
@@ -1,19 +1,23 @@
 import java.awt.Color;
+import java.awt.Font;
 
 import javax.swing.JFrame;
-import javax.swing.JTextPane;
+import javax.swing.JLabel;
 
 public class BigWords {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Big Words");
 
-        JTextPane textPane = new JTextPane();
+        JLabel textLabel = new JLabel("Hello");
+        textLabel.setHorizontalAlignment(JLabel.CENTER);
+        textLabel.setFont(new Font("SansSerif", Font.BOLD, 90));
+        textLabel.setForeground(Color.WHITE);
 
-        frame.add(textPane);
+        frame.add(textLabel);
 
         frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
         frame.setUndecorated(true);
-        frame.setBackground(Color.WHITE);
+        frame.getContentPane().setBackground(Color.BLACK);
 
         frame.setVisible(true);
     }
